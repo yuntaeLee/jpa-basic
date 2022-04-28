bed6099a457925e838333f74b46fc4d5eb8c9003
yuntaeLee <leeyt1201@gmail.com>
2022-04-29 02:33:49 +0900

    temp

---
+++ association-mapping/src/main/java/oneway/Member.java
@@ -0,0 +1,22 @@
+package oneway;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+}
