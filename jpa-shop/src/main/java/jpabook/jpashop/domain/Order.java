7450c7d3dfcf0ac5107dff8ddeaafb3143be5810
yuntaeLee <leeyt1201@gmail.com>
2022-05-06 01:10:53 +0900

    Refactor: association mapping

--- jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -15,11 +17,26 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    /**
+     * 연관관계 편의 메서드
+     */
+    public void addOrderIItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
