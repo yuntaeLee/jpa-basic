b70d637bc64b1d4414ec4ebb7ac3e401d968efec
yuntaeLee <leeyt1201@gmail.com>
2022-05-18 06:44:41 +0900

    Extends BaseEntity

--- jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-shop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter @Setter
 @Table(name = "ORDERS")
-public class Order {
+public class Order extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
