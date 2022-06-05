8b0bfe30f1311cc755b60e8528288312de291afc
yuntaeLee <leeyt1201@gmail.com>
2022-06-06 06:01:41 +0900

    Type expression

---
+++ jpql-first/src/main/java/jpql/TypeExpressionMain.java
@@ -0,0 +1,71 @@
+package jpql;
+
+import jpql.entity.Member;
+import jpql.entity.MemberType;
+import jpql.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class TypeExpressionMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            /**
+             * JPQL 타입 표현
+             * - 문자: 'HELLO', 'She"s'
+             * - 숫자: 10L(Long), 10D(Double), 10F(Float)
+             * - Boolean: true, false
+             * - ENUM: jpql.entity.MemberType.ADMIN
+             * - 엔티티 타입: TYPE(m) = Member (상속 관계에서 사용)
+             */
+            String query = "select m.username, 'HELLO', true from Member m " +
+                    "where m.type = :userType";
+            List<Object[]> result = em.createQuery(query)
+                    .setParameter("userType", MemberType.ADMIN)
+                    .getResultList();
+
+            for (Object[] objects : result) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
